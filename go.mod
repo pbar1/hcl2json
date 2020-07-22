00c601cd03c40f78652394a06dff0635afea89c2
Pierce Bartine <pbartine@salesforce.com>
2020-07-22 13:56:11 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/pbar1/hcl2json
+
+go 1.14
+
+require (
+	github.com/hashicorp/hcl v1.0.0
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/spf13/viper v1.7.0 // indirect
+	gopkg.in/yaml.v2 v2.2.4
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/hashicorp/hcl"
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	outputYAML := false
+	for _, arg := range os.Args {
+		if arg == "-h" || arg == "--help" || arg == "-help" {
+			fmt.Println(`Converts Hashicorp Configuration Langauge (HCL) to JavaScript Object Notation (JSON).
+Expects input from stdin.
+
+Usage:
+  cat *.tf | hcl2json
+
+Flags:
+  -h, --help   help for hcl2json
+  -y, --yaml   output YAML`)
+			os.Exit(0)
+		}
+		if arg == "-y" || arg == "--yaml" || arg == "-yaml" {
+			outputYAML = true
+
+		}
+	}
+
+	in, err := ioutil.ReadAll(os.Stdin)
+	check(err)
+
+	var v interface{}
+	err = hcl.Unmarshal(in, &v)
+	check(err)
+
+	var out []byte
+	if outputYAML {
+		out, err = yaml.Marshal(v)
+	} else {
+		out, err = json.Marshal(v)
+	}
+	check(err)
+
+	fmt.Println(string(out))
+}
+
+func check(err error) {
+	if err != nil {
+		if _, err := fmt.Fprintln(os.Stderr); err != nil {
+			panic(err)
+		}
+		os.Exit(1)
+	}
+}
